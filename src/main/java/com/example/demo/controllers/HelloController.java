f9a28a827727d056c51f8f13e66c6494e1b529d9
qgorak <q.gorak@gmail.com>
2020-09-14 11:36:33 +0200

    implementing classes from poo1-java

--- src/main/java/com/example/demo/controllers/HelloController.java
+++ src/main/java/com/example/demo/controllers/HelloController.java
@@ -1,6 +1,12 @@
 package com.example.demo.controllers;
+import com.example.demo.models.Group;
+import com.example.demo.models.Organization;
+import com.example.demo.models.User;
+
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -12,8 +18,13 @@ public @ResponseBody String hello() {
 	return "Hello";
 }
 
-@RequestMapping("/v")
-public String HelloWithView() {
+@RequestMapping("/v/{name}")
+public String HelloWithView(@PathVariable String name,ModelMap model) {
+	Organization orga=new Organization(name);
+	Group group=new Group("BTS2A",orga);
+	new User("quentin", group);
+	new User("sam", group);
+	model.addAttribute("orga",orga);
 	return "index";
 }
 }
---
+++ src/main/java/com/example/demo/models/User.java
@@ -0,0 +1,60 @@
+package com.example.demo.models;
+
+
+public class User extends Object{
+	private String login;
+	private String password;
+	private Group groupe;
+//	private Organization orga;
+	
+	public User(String login, Group groupe) {
+		this.login=login;
+		this.groupe=groupe;
+		this.groupe.addUser(this);
+
+	}
+	public String getLogin()	{
+		return this.login;
+	}
+	public void setLogin(String login)	{
+		this.login=login;
+	}
+	public String getPassword() {
+		return this.password;
+	}
+	public void setPassword(String password, String conf) throws Exception {
+		if (password.equals(conf) && !password.equals(this.password)) {
+			this.password = password;
+		}
+		else{
+
+		}
+	}
+	public Group getGroup() {
+		return groupe;
+	}
+	public void setGroup(Group groupe) {
+		this.groupe = groupe;
+	}
+
+	public Organization getOrganization() {
+		return groupe.getOrga();
+	}
+//	public Organization getOrga() {
+//		return orga;
+//	}
+//	public void setOrga(Organization orga) {
+//		this.orga = orga;
+//	}
+	@Override
+	public String toString() {
+		if (groupe!=null) {
+			return login+"("+password+")"+groupe;
+		}
+		else{
+			
+			return login+"("+password+")";
+		}
+	}
+}
+
